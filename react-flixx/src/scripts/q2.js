a146be2a92fdc3ff82c06c28d22dc259c51df845
Eazy-creator <82000640+Eazy-creator@users.noreply.github.com>
2022-10-03 16:39:33 +0100

    Update q2.js

--- react-flixx/src/scripts/q2.js
+++ react-flixx/src/scripts/q2.js
@@ -1,16 +1,27 @@
-function postForm(){
-    var form = new FormData();
-    form.append("films", films);
-    axios.post('https://api.flixpremiere.com/v1/films/filter/now_showing?limit=10',form)
-      .then((response) => {
-        console.log('sss',response.data.data);
-        if(response.data.data){
-          setData(response.data.data);
+function getFilms() {
+  let xhr = new XMLHttpRequest();
+
+  try {
+    xhr.open("GET", "https://api.flixpremiere.com/v1/films/filter/now_showing?limit=10");
+
+    xhr.send();
+
+    let jsonFilms = {};
+    xhr.onload = () => {
+      jsonFilms = JSON.parse(xhr.responseText);
+      jsonFilms = jsonFilms.films;
+      let eligibleFilms = [];
+
+      for (let jsonFilm of jsonFilms) {
+        if (jsonFilm.duration_seconds >= 5500) {
+          eligibleFilms.push(jsonFilm);
+          console.log(jsonFilm.title + " (" + jsonFilm.duration_seconds + ")");
         }
-    });
+      }
+    }
+  } catch(error) {
+    console.log(error);
   }
-  
-  useEffect(() => {
-    const timer = setInterval(() => postForm, 5000);
-    return () => clearInterval(timer);
-  },[]);
+  setTimeout(getFilms, 5000);
+}
+getFilms();
